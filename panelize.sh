9f1955a9bf94acd97516b305ea2d98ee40026d54
Patrick Coleman <blinken@gmail.com>
2023-09-05 13:27:31 +0100

    v1.0 gerbers as-manufactured
    Schematic and PCB verification completed and corrections made. Add BOM,
    excluding THT components, and CPL file for automated assembly.
    
    Add script that adds 5mm edge rails to a "panel" for assembly, and add
    the panel.

---
+++ panelize.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+kikit panelize  \
+  --layout 'grid; rows: 1; cols: 1; space: 3mm;' \
+  --source 'tolerance: 15mm' \
+  --cuts vcuts \
+  --framing 'frame; width: 5mm; space: 0mm;' \
+  --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
+  --fiducials '3fid; hoffset: 10mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
+  --text 'simple; text: low noise power probe v1.0; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+  --post 'millradius: 1mm' \
+  low-noise-power-probe.kicad_pcb low-noise-power-probe-panel.kicad_pcb

d1c7ff8becdb0172f09806da3bc7e10880e7b36c
Patrick Coleman <blinken@gmail.com>
2025-07-08 11:40:57 +0100

    add panel and gerbers for v2.0

--- panelize.sh
+++ panelize.sh
@@ -7,6 +7,6 @@ kikit panelize  \
   --framing 'frame; width: 5mm; space: 0mm;' \
   --tooling '3hole; hoffset: 2.5mm; voffset: 2.5mm; size: 1.5mm' \
   --fiducials '3fid; hoffset: 10mm; voffset: 2.5mm; coppersize: 2mm; opening: 1mm;' \
-  --text 'simple; text: low noise power probe v1.0.1; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
+  --text 'simple; text: low noise power probe v2.0 JLCJLCJLCJLC; anchor: mt; voffset: 2.5mm; hjustify: center; vjustify: center;' \
   --post 'millradius: 1mm' \
   low-noise-power-probe.kicad_pcb low-noise-power-probe-panel.kicad_pcb
